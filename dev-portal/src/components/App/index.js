704d92761e747929d16cba1b993ebfbd358d784b
Riza Nugraha <rnugraha@gmail.com>
2018-04-18 18:53:07 +0200

    Refactor homepage and header
    Initial refactoring on homepage and header to have Quisper look and feel.

--- dev-portal/src/components/App/index.js
+++ dev-portal/src/components/App/index.js
@@ -1,19 +1,37 @@
-import React, { PureComponent } from 'react'
-import { BrowserRouter, Route, Redirect, Link, Switch } from 'react-router-dom'
-import { Dimmer, Loader } from 'semantic-ui-react'
+/**
+ * Copyright (c) 2018 The Hyve B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, {PureComponent} from 'react'
+import {BrowserRouter, Route, Redirect, Link, Switch} from 'react-router-dom'
+import {Dimmer, Loader, Segment} from 'semantic-ui-react'
 import Home from '../../pages/Home'
 import CaseStudies from '../../pages/CaseStudies'
 import GettingStarted from '../../pages/GettingStarted'
 import Dashboard from '../../pages/Dashboard'
 import Apis from '../../pages/Apis'
 import ApiDetails from '../../pages/ApiDetails'
-import logo from '../../logo.png'
 import AlertPopup from '../../components/AlertPopup'
-import { init, isAuthenticated } from '../../services/self'
-import { apiGatewayClient } from '../../services/api'
+import {init, isAuthenticated} from '../../services/self'
+import {apiGatewayClient} from '../../services/api'
 import './App.css'
+import Footer from "../Footer";
+import QspHeader from "../QspHeader";
 
 const NoMatch = () => <h2>Page not found</h2>;
+
 class MatchWhenAuthorized extends PureComponent { // eslint-disable-line
   constructor(props) {
     super(props);
@@ -36,16 +54,17 @@ class MatchWhenAuthorized extends PureComponent { // eslint-disable-line
     const {component: Component, ...rest} = this.props;
 
     return <Route {...rest} render={props => {
-      if (!isAuthenticated()) return <Redirect to={{ pathname: '/', state: { from: props.location } }}/>;
+      if (!isAuthenticated()) return <Redirect to={{pathname: '/', state: {from: props.location}}}/>;
 
-      return this.state.apiGatewayClient ? <Component {...props} />: (<Dimmer active>
-        <Loader content='Loading' />
+      return this.state.apiGatewayClient ? <Component {...props} /> : (<Dimmer active>
+        <Loader content='Loading'/>
       </Dimmer>)
-    }} />
+    }}/>
   }
 }
 
 export default class App extends PureComponent {
+
   constructor() {
     super();
     init();
@@ -61,28 +80,24 @@ export default class App extends PureComponent {
     return (
       <BrowserRouter>
         <div className="App">
-          <div className="App-header">
-            <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
-            <h2>Welcome to our Developer Portal</h2>
-          </div>
           <section className="App-intro">
-              <AlertPopup />
+            <AlertPopup/>
+            <QspHeader/>
+            <Segment className='App-body' basic>
               <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/case-studies" component={CaseStudies} />
-                <Route path="/getting-started" component={GettingStarted} />
+                <Route exact path="/" component={Home}/>
+                <Route path="/case-studies" component={CaseStudies}/>
+                <Route path="/getting-started" component={GettingStarted}/>
                 <Route path="/dashboard" component={Dashboard}/>
                 <Route exact path="/apis" component={Apis}/>
                 <Route path="/apis/:apiId" component={ApiDetails}/>
                 <Route component={NoMatch}/>
               </Switch>
+            </Segment>
           </section>
+          <Footer/>
         </div>
       </BrowserRouter>
     )
-    /*
-
-
-     */
   }
 }
---
+++ dev-portal/src/components/QspHeader/index.js
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2018 The Hyve B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, {PureComponent} from 'react'
+import QspMenu from "../QspMenu";
+import {Header, Segment, Button} from "semantic-ui-react";
+import SignIn from "../SignIn";
+import Register from "../Register";
+import {isAuthenticated} from "../../services/self";
+import {confirmMarketplaceSubscription} from "../../services/api-catalog";
+import {getQueryString} from "../../services/misc";
+import {Menu} from 'semantic-ui-react'
+import {Image} from 'semantic-ui-react'
+import logo from '../QspMenu/logo.png'
+
+export default class QspHeader extends PureComponent {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      isAuthenticated: isAuthenticated()
+    };
+
+    // TODO: Code below are legacy. To find out what does it mean in relation with AWS.
+    // ********************************************************************************
+    const {usagePlanId, token} = getQueryString();
+
+    if (usagePlanId && token) {
+      this.state = {usagePlanId, token};
+      if (this.state.isAuthenticated) {
+        confirmMarketplaceSubscription(usagePlanId, token).then(() => {
+          window.location.href = '/apis'
+        })
+      }
+    }
+    // ********************************************************************************
+  }
+
+  handleSignIn(d) {
+    this.setState({isAuthenticated: d.signedIn});
+  }
+
+  renderLogin() {
+    return (
+      <Segment textAlign='center' vertical className='App-segment'>
+        <Menu className='QspMenu' secondary>
+          <Menu.Item name='browse'>
+            <Image src={logo} wrapped/>
+          </Menu.Item>
+        </Menu>
+        <br/>
+        <Header>
+          Welcome to Developer Portal &nbsp;
+          <h1>Information service for personalized nutrition and lifestyle advice</h1>
+          <Button.Group>
+            <SignIn usagePlanId={this.state.usagePlanId} token={this.state.token}
+                    onChange={(d) => this.handleSignIn(d)}/>
+            <Button.Or/>
+            <Register usagePlanId={this.state.usagePlanId} token={this.state.token}/>
+          </Button.Group>
+        </Header>
+      </Segment>
+    );
+  }
+
+  renderToolbar() {
+    return <QspMenu onChange={(d) => this.handleSignIn(d)}/>;
+  }
+
+  render() {
+    return this.state.isAuthenticated ? this.renderToolbar() : this.renderLogin();
+  }
+}
