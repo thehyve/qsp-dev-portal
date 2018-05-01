92d1915059da6ad3f78d479a7a541588d3528ccc
Joris Borgdorff <joris@thehyve.nl>
2018-05-01 16:39:55 +0200

    Remove unnecessary apidetails introduction

--- dev-portal/src/pages/ApiDetails/index.js
+++ dev-portal/src/pages/ApiDetails/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { Dimmer, Loader, Button } from 'semantic-ui-react'
+import { Dimmer, Loader} from 'semantic-ui-react'
 import { getApi } from '../../services/api-catalog'
 import Head from '../../components/Head'
 import SwaggerUI from "../../components/SwaggerUI";
@@ -21,18 +21,10 @@ export default class ApiDetailsPage extends PureComponent {
     return (<div>
       <Head {...this.props} />
       <section className="swagger-section" style={{overflow: 'auto'}}>
-        {this.state.api ?
-          (<div>        <a href = {this.state.api.extraDoc} target="_blank" rel="noopener noreferrer">
-          <Button >Show PDF</Button>
-        </a>
-
-        <p>Access this API from the base URL https://{this.state.api.swagger.host}{this.state.api.swagger.basePath}, providing
-        your API key in the <code>x-api-key</code> header.</p>
-            <SwaggerUI spec={this.state.api.swagger}/>
-          </div>) : (<Dimmer active>
-          <Loader content='Loading' />
+        {this.state.api ? <SwaggerUI spec={this.state.api.swagger}/> : (
+          <Dimmer active>
+            <Loader content='Loading' />
           </Dimmer>)}
-
       </section>
     </div>)
   }
