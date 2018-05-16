ecd0e0f33ff32e70fe2ed71f3147d56cca2cf8da
Riza Nugraha <rnugraha@gmail.com>
2018-05-16 14:40:46 +0200

    Add setup tests file

--- dev-portal/src/components/App/App.test.js
+++ dev-portal/src/components/App/App.test.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import App from "./index";
-import renderer from "react-test-renderer";
 
 test('renders without crashing', () => {
   const testRenderer = renderer.create(
