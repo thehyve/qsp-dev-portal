c8f15223ca4b902adac614de92ccad78c6595eea
Joris Borgdorff <joris@thehyve.nl>
2018-05-22 11:49:38 +0200

    Fixed react-loadable loading property

--- dev-portal/src/components/SwaggerUI/index.js
+++ dev-portal/src/components/SwaggerUI/index.js
@@ -4,5 +4,5 @@ import {Dimmer, Loader} from "semantic-ui-react";
 
 export default Loadable({
   loader: () => import('./ActualSwaggerUI'),
-  loading: () => props => props.pastDelay ? <Dimmer active inverted><Loader content='Loading'/></Dimmer> : null,
+  loading: props => props.pastDelay ? <Dimmer active inverted><Loader content='Loading'/></Dimmer> : null,
 });
