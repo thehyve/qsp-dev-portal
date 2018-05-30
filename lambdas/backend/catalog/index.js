9846ac304594ea6011e302dc5a53c42481ee6da1
Joris Borgdorff <joris@thehyve.nl>
2018-05-30 17:03:22 +0200

    Updated service descriptions

--- lambdas/backend/catalog/index.js
+++ lambdas/backend/catalog/index.js
@@ -16,6 +16,7 @@ const usagePlans = [
         id: '5iz79asre6',
         image: '/sam-logo.png',
         swagger: drvDef,
+        summary: 'Service that provides dietary reference values compiled by EuroFIR.',
         extraDoc: '/pdfs/drv.pdf',
       },
     ],
@@ -28,6 +29,7 @@ const usagePlans = [
         id: 'gms327o2ak',
         image: '/sam-logo.png',
         swagger: foodTransport,
+        summary: 'SOAP web service that provides access to food composition data collected by EU project EuroFIR.',
         extraDoc: '/pdfs/FoodTransport.pdf'
       },
     ],
@@ -40,6 +42,7 @@ const usagePlans = [
         id: 'tnrl2m4nkf',
         image: '/sam-logo.png',
         swagger: harmonisedQuisper,
+        summary: 'An XML web service that provides a single-point access to QSP webservices.',
         extraDoc: '/pdfs/HarmonizedQuisper.pdf'
       },
     ],
@@ -52,6 +55,7 @@ const usagePlans = [
         id: 'j3ctrwbieg',
         image: '/sam-logo.png',
         swagger: pdasDoc,
+        summary: 'Web-service based on the Food4Me level 3 advices.',
         extraDoc: '/pdfs/pdas.pdf'
       },
     ],
